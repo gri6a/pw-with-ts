84d4e5600ea9580d56dc065ab7bac058c1b6563e
gri6a <gw2803@gmail.com>
2025-05-28 20:29:53 +0100

    enabling firefox and webkit web browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,19 +44,19 @@ export default defineConfig({
       dependencies: ["setup"],
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'],  storageState: '.auth/user.json' },
-    // dependencies: ["setup"],
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'],  storageState: '.auth/user.json' },
+    dependencies: ["setup"],
 
-    // },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'],  storageState: '.auth/user.json' },
-    // dependencies: ["setup"],
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'],  storageState: '.auth/user.json' },
+    dependencies: ["setup"],
 
-    // },
+    },
 
     /* Test against mobile viewports. */
     // {
