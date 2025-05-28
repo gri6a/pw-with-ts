ecb4a43aa3c9c671fdb1f7044c930c6f36e084e7
gri6a <gw2803@gmail.com>
2025-05-28 14:38:48 +0100

    adding mock article test

--- tests/conduitAPI.spec.ts
+++ tests/conduitAPI.spec.ts
@@ -2,17 +2,24 @@ import { test, expect } from "@playwright/test";
 // import tags from '../test-data/tags.json';
 const tags = require("../test-data/tags.json");
 test.beforeEach("Navigate to base url", async ({ page }) => {
-  await page.route(
-    "*/**/api/tags",
-    async (route) => {
+  await page.route("*/**/api/tags", async (route) => {
     //   const tags = {
     //     tags: ["Automation", "Mocked API"],
     //   };
-      await route.fulfill({
-        body: JSON.stringify(tags),
-      });
-    }
-  );
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+  await page.route("*/**/api/articles*", async (route) => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = "Title updated";
+    responseBody.articles[0].description = "Description updated";
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  });
   await page.goto("https://conduit.bondaracademy.com/", {
     waitUntil: "networkidle",
   });
@@ -20,8 +27,13 @@ test.beforeEach("Navigate to base url", async ({ page }) => {
 
 test("test1 - Mocking API", async ({ page }) => {
   await expect(page).toHaveURL("https://conduit.bondaracademy.com/");
+  // Assertion for mock tags
   await expect(page.locator(".sidebar .tag-default.tag-pill")).toHaveText([
     "Automation",
     "Mocked API",
   ]);
+  // Assertion for mock article
+  await expect(page.locator('app-article-preview h1').first()).toHaveText('Title updated')
+  await expect(page.locator('app-article-preview p').first()).toHaveText('Description updated')
+
 });
