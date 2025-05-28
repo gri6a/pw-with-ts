eae3dc52cf508004021f7321fef1ab44113cd763
gri6a <gw2803@gmail.com>
2025-05-28 16:40:31 +0100

    delete an article via intercepting API call test

--- tests/conduitAPI.spec.ts
+++ tests/conduitAPI.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, request } from "@playwright/test";
+import { ADDRGETNETWORKPARAMS } from "dns";
 // import tags from '../test-data/tags.json';
 const tags = require("../test-data/tags.json");
 test.beforeEach("Navigate to base url", async ({ page }) => {
@@ -85,4 +86,60 @@ test("Deleting an article", async ({ page, request }) => {
   await expect(page.locator("app-article-preview h1").first()).not.toHaveText(
     "titlev2"
   );
+});
+
+test("Create an article via UI - Intercept browser API response - Delete an artivle via API", async ({
+  page,
+  request,
+}) => {
+  /*
+  step1: Login
+  step2: Create an article
+  step3: Validate that article created as expected
+  step4: Clean the result of this test from the backend server, so we can reuse this test
+  Delete an article via API call
+  */
+  await page.getByText(" New Article ").click();
+  await expect(page).toHaveURL("https://conduit.bondaracademy.com/editor");
+  await page
+    .getByPlaceholder("Article Title")
+    .fill("Candidate for deletion via API call");
+  await page.getByPlaceholder("What's this article about?").fill("Delete call");
+  await page.getByPlaceholder("Write your article (in markdown)").fill("abc");
+  await page.getByRole("button", { name: " Publish Article " }).click();
+  // Intercepting the API call
+  const createArticleResponse = await page.waitForResponse(
+    "https://conduit-api.bondaracademy.com/api/articles/"
+  );
+  const createArticleResponseBody = await createArticleResponse.json();
+  const slugId = createArticleResponseBody.article.slug;
+
+  await expect(page.locator(".article-page .banner h1")).toContainText(
+    "Candidate for deletion via API call"
+  );
+  await page.locator(".navbar-brand").click();
+  await page.getByText("Global Feed").click();
+  await expect(page).toHaveURL("https://conduit.bondaracademy.com/");
+  await expect(
+    page.locator(".article-preview .preview-link h1").first()
+  ).toContainText("Candidate for deletion via API call");
+
+  const response = await request.post(
+    "https://conduit-api.bondaracademy.com/api/users/login",
+    {
+      data: { user: { email: "tester2803@test.com", password: "Tester123" } },
+    }
+  );
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const deleteArticleResponse = await request.delete(
+    `https://conduit-api.bondaracademy.com/api/articles/${slugId}`,
+    {
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    }
+  );
+  expect(deleteArticleResponse.status()).toEqual(204);
 });
